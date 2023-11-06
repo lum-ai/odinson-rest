63a4b101134548f5bd8b39a8d8a0903789e7c81f
Gus Hahn-Powell <gushahnpowell@gmail.com>
2023-11-05 17:32:17 -0700

    Test property access

--- python/lum/odinson/tests/test_odinson_document.py
+++ python/lum/odinson/tests/test_odinson_document.py
@@ -9,8 +9,16 @@ import pytest
 # see https://docs.python.org/3/library/unittest.html#basic-example
 class TestOdinsonDocument(unittest.TestCase):
     def test_load_from(self):
-        """odinson.Document.from_file() should load an odinson.Document from a path to a Odinson Document JSON file."""
+        """odinson.Document.from_file() should load an odinson.Document from a path to an Odinson Document JSON file."""
         od = odinson.Document.from_file(TEST_DOC_PATH)
         self.assertTrue(
             isinstance(od, odinson.Document), f"{type(od)} was not an odinson.Document"
+        )
+
+    def test_property_access(self):
+        """odinson.Document should store token attributes for easy access via doc.attribute_name.
+        """
+        od = odinson.Document.from_file(TEST_DOC_PATH)
+        self.assertTrue(
+            len(od.lemma) > 0, f"doc.lemma should not be empty, but returned {od.lemma}"
         )
